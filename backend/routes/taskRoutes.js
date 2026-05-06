3b5bad40332ce8d7e8a031fd3e97575b9037c443
abirtaghi <taghi.abir@etu.uae.ac.ma>
2026-05-06 21:54:47 +0100

    feat: implement task CRUD routes

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const Task = require("../models/Task");
+router.post("/", async (req, res) => {
+    try{
+        const task = await Task.create(req.body);
+        res.status(201).json(task);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+router.get("/", async(req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.json(tasks);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+router.put("/:id", async (req, res) => {
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true,});
+        res.json(task);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+router.delete("/:id", async (req, res) => {
+    try {
+        await Task.findByIdAndDelete(req.params.id);
+        res.json({ message: "Task supprimée"});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+module.exports = router;
