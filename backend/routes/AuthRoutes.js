42a5535e96eb889d0f8b82cb71c059a41aaec71f
aya-hassani <aya.hassanilhsinui@etu.uae.ac.ma>
2026-05-21 03:11:12 +0100

    feat(auth): define login and register api routes

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,1 +1,13 @@
-﻿
+﻿const express = require('express');
+const router = express.Router();
+const { register, login, getMe } = require('../controllers/authController');
+
+// Route pour l'inscription : POST http://localhost:5000/api/auth/register
+router.post('/register', register);
+
+// Route pour la connexion : POST http://localhost:5000/api/auth/login
+router.post('/login', login);
+
+// Note : La route /me (profil) sera ajoutée dès qu'on aura écrit le middleware de sécurité !
+
+module.exports = router;
