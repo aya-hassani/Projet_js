3e691e70b82c24493239242c43745c7080e44fbf
Oumnia2004-zeyouti <oumnia.zeyouti@etu.uae.ac.ma>
2026-05-21 17:25:19 +0200

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
-﻿
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const authRoutes = require('./routes/authRoutes');
+const projectRoutes = require('./routes/projectRoutes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ MongoDB connecté'))
+  .catch(err => console.log('❌ MongoDB error:', err));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API TaskFlow fonctionne!' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur démarré sur http://localhost:${PORT}`);
+});﻿
