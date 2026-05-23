f7ac71fceca128ff849856656cbc8ee94ebeb2d1
Oumnia2004-zeyouti <oumnia.zeyouti@etu.uae.ac.ma>
2026-05-23 06:21:32 +0200

    Update AuthRoutes.js

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,13 +1,8 @@
-﻿const express = require('express');
+const express = require('express');
 const router = express.Router();
-const { register, login, getMe } = require('../controllers/authController');
-const { protect } = require('../middleware/authMiddleware'); // <-- On importe le vigile
+const { register, login } = require('../controllers/authController');
 
-// Routes publiques
 router.post('/register', register);
 router.post('/login', login);
-
-// Route privée (protégée par le middleware)
-router.get('/me', protect, getMe); // <-- Seul un utilisateur connecté peut y accéder
 
 module.exports = router;
